25b7033833bc85befe759fd911c43c5d8de869d7
shihab-dot-dev <shihab.pm.dev@gmail.com>
2022-03-07 12:45:08 +0530

    read me file updated plus vitejs server config changed

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,16 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  /* 
+    if you want to run localhost without ssl cert
+    remove server property `https` and `mkcert()` from plugin property
+    then run `npm run serve` npm script 
+  */
   server: {
     https: true,
   },
-  plugins: [mkcert()],
+  plugins: [{ ...mkcert() }],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
